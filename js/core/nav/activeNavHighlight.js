c67ec47b3ac05a73cf9b7c018be7327413672d93
KiruthikaMKS1617 <mksdevdesk@gmail.com>
2026-03-19 16:04:34 +0530

    refactor: enhance visibility tracking in activeNavHighlight with IntersectionObserver initialSnapshot

--- js/core/nav/activeNavHighlight.js
+++ js/core/nav/activeNavHighlight.js
@@ -29,32 +29,50 @@ const activeNavHighlight = () => {
   applyFromHash();
   globalThis.addEventListener("hashchange", applyFromHash);
 
-  const isMobile = globalThis.matchMedia("(max-width: 768px)").matches;
+  const media = matchMedia("(max-width: 768px)");
+  let isMobile = media.matches;
+
+  media.addEventListener("change", (e) => {
+    isMobile = e.matches;
+  });
 
   const observerOptions = isMobile
     ? { rootMargin: "-25% 0px -25% 0px", threshold: 0.15 }
     : { rootMargin: "0px 0px -50% 0px", threshold: 0.2 };
 
+  const visibleMap = new Map();
+
   const observer = new IntersectionObserver((entries) => {
     if (isHashPriority) return;
 
-    const visibleSections = entries
-      .filter((e) => e.isIntersecting)
-      .sort((a, b) =>
-        isMobile
-          ? b.boundingClientRect.top - a.boundingClientRect.top
-          : a.boundingClientRect.top - b.boundingClientRect.top,
-      );
+    entries.forEach((entry) => {
+      visibleMap.set(entry.target.id, entry.isIntersecting);
+    });
+    // entries = array of sections that have changes in visibility (Eg: from visible to hidden or vice versa)
+    //“First time → set initial values --- observer runs immediately with the current visibility of all sections first time it is created
+    // After that → track updates” --- for sections that undergoes visibility changes.
+    // Eg: visible to hidden and vice versa. Only those sections will be in entries,
+    //  not all sections. So we update the visibleMap with the current visibility
+    // status of those sections that are in entries. Hence, we can access all visible
+    // sections for those whose visibility has changed (with this update)
+    // and has not changed (from the initial snapshot)
+    // ********************
+    // IntersectionObserver = initial snapshot + incremental updates
 
-    if (
-      !visibleSections.length ||
-      !document.getElementById(visibleSections[0].target.id)
-    )
-      return;
+    const visibleSections = [...visibleMap.entries()]
+      .filter(([_, isVisible]) => isVisible)
+      .map(([id]) => document.getElementById(id));
 
-    setActiveLink(visibleSections[0].target.id);
+    if (!visibleSections.length) return;
+
+    const sorted = visibleSections.toSorted((a, b) =>
+      isMobile
+        ? b.getBoundingClientRect().top - a.getBoundingClientRect().top
+        : a.getBoundingClientRect().top - b.getBoundingClientRect().top,
+    );
+
+    setActiveLink(sorted[0].id);
   }, observerOptions);
-
   sections.forEach((section) => observer.observe(section));
 };
 
